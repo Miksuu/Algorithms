2657238e93ce143bcbf50b2963fba0181da53ab0
Miksuu <imiksuu96@gmail.com>
2023-09-28 23:07:44 +0300

    Add linear search algorithm, programming task 21 done.

--- programmingTasks21-22.cpp
+++ programmingTasks21-22.cpp
@@ -3,12 +3,39 @@
 #include <chrono>
 #include "ArrayTools.cpp"
 
-void ProgrammingTask21() {
+bool linearSearch(int _numberToSearchFor, const int* _arrayToSearchFrom, int _arraySize) {
+    for (int i = 0; i < _arraySize; ++i) {
+        if (_arrayToSearchFrom[i] == _numberToSearchFor) {
+            return true;
+        }
+    }
+    return false;
 }
 
+void ProgrammingTask21(int _testCasesCount) {
+    int arrayToSearchFrom[] = {1, 4, 6, 11, 13, 16, 19, 20, 25, 27, 29, 30, 32, 36, 39, 42, 45, 48, 49, 53};
+    int arrayLength = sizeof(arrayToSearchFrom) / sizeof(arrayToSearchFrom[0]);
+    ArrayTools::printArray("Array: ", arrayToSearchFrom, arrayLength);
+
+    // Re-using some code from previous assignment, find max element to define the max number to search for during the test cases (min from 0)
+    int maxElementToSearchFor = ArrayTools::findMaxElementInArray(arrayToSearchFrom, arrayLength);
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::mt19937 generator(seed);
+    std::uniform_int_distribution<int> distribution(0, maxElementToSearchFor);
+
+    // Running the test cases with linear search
+    for (int tc = 0; tc < _testCasesCount; ++tc) {
+        int numberToSearchFor = distribution(generator);
+        bool found = linearSearch(numberToSearchFor, arrayToSearchFrom, arrayLength);
+
+        std::cout <<"TC: " << tc << " | numberToSearchFor " << numberToSearchFor << " found: " << std::boolalpha << found << std::endl;
+    }
+    std::cout << "\n";
+}
 
 int main() {
-    ProgrammingTask21();
+    // Define the number of test cases here
+    ProgrammingTask21(10);
     //ProgrammingTask22();
 
     return 0;
