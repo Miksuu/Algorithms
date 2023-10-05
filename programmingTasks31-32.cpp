0fd3d48428f48cbc04bc1e89046dce13ce461847
Miksuu <imiksuu96@gmail.com>
2023-10-05 20:44:55 +0300

    Add loopUntil param

--- programmingTasks31-32.cpp
+++ programmingTasks31-32.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include "ArrayTools.cpp"
 
-void ProgrammingTask31() {
+void ProgrammingTask31(int _loopUntil) {
     int first = 0;
     int second = 1;
     int next = 0;
 
-    for (int i = 2; i <= 1000; ++i) {
+    for (int i = 2; true; ++i) {
         next = first + second;
         first = second;
         second = next;
+
         std::cout << second << "\n";
+
+        if (second >= _loopUntil) {
+            break;
+        }
     }
 }
 
@@ -20,7 +25,7 @@ void ProgrammingTask32() {
 
 int main() {
     // Define the number of test cases for each of the linear and binary searches here
-    ProgrammingTask31();
+    ProgrammingTask31(13337);
     ProgrammingTask32();
 
     return 0;
